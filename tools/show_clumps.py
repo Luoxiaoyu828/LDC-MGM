ff7d4682d0d9397b5b265d49f3a70d9707eacdee
Luoxiaoyu <202108080011014@ctgu.edu.cn>
2022-06-27 18:52:07 +0800

    增加画图时根据fig_name来选择是否保存图片

--- tools/show_clumps.py
+++ tools/show_clumps.py
@@ -129,137 +129,7 @@ def make_plot_wcs(data_name, loc_outcat_wcs_name, outcat_wcs_name):
     cbar = fig.colorbar(im0, cax=axes1)
     cbar.set_label('K m s${}^{-1}$')
 
-    plt.savefig(fig_name)
-    # shutil.copy(fig_name, os.path.join(r'R2_data\hh', title + '.png'))
-    plt.close()
-
-
-def deal_data(path_detect, path_save_fig, loc=False):
-    """
-
-    对检测的结果，将每个云核进行可视化
-    处理数据并调用 make_plot_11()画图
-
-    :param path_detect: 一个Cell检测结果保存的路径
-    :param path_save_fig: 对云核可视化的保存路径
-    :return:
-    """
-
-    """
-    :param path_outcat: 像素核表
-    :param path_outcat_wcs: 经纬度核表
-    :param path_data: 数据
-    :param path_mask: 掩膜
-    :param path_save_fig: 图片保存路径
-    :param v_len: 谱线速度轴的1/2（自己定义）
-    :return:
-    """
-    if not os.path.exists(path_save_fig):
-        os.mkdir(path_save_fig)
-
-    v_len = 30
-    if loc:
-        path_outcat = os.path.join(path_detect, 'LDC_auto_loc_outcat.csv')
-        path_outcat_wcs = os.path.join(path_detect, 'LDC_auto_loc_outcat_wcs.csv')
-    else:
-        path_outcat = os.path.join(path_detect, 'LDC_auto_outcat.csv')
-        path_outcat_wcs = os.path.join(path_detect, 'LDC_auto_outcat.csv')
-
-    path_mask = os.path.join(path_detect, 'LDC_auto_mask.fits')
-    detect_log = os.path.join(path_detect, 'LDC_auto_detect_log.txt')
-    f = open(detect_log)
-    file_name = [item for item in f.readlines() if item.count('fits')]
-    f.close()
-    file_name = file_name[0].split(' ')[-1][:-1]
-    path_data = file_name
-
-    table_LDC = pd.read_csv(path_outcat, sep='\t')  # 像素核表
-    table_LDC_wcs = pd.read_csv(path_outcat_wcs, sep='\t')  # 经纬度核表
-
-    data = fits.getdata(path_data)  # 数据
-    mask = fits.getdata(path_mask)  # 掩膜
-    [size_v, size_y, size_x] = mask.shape
-
-    expend = 1
-    for item_i, item in enumerate(table_LDC['ID']):
-        item = int(item)
-        peak1 = table_LDC['Peak1'][item_i]
-        peak2 = table_LDC['Peak2'][item_i]
-        peak3 = table_LDC['Peak3'][item_i]
-        cen3 = table_LDC['Cen3'][item_i]
-        title_name = table_LDC_wcs['ID'][item_i]
-
-        mask1 = mask.copy()
-        mask1[mask1 != item] = 0
-        clump_item = data * mask1 / item
-        [a, b, c] = np.where(clump_item != 0)
-        aa = np.array([[a.min() - expend, 0], [a.max() + 1 + expend, size_v]])
-        bb = np.array([[b.min() - expend, 0], [b.max() + 1 + expend, size_y]])
-        cc = np.array([[c.min() - expend, 0], [c.max() + 1 + expend, size_x]])
-        clump_item_loc = clump_item[aa[0].max(): aa[1].min(), bb[0].max(): bb[1].min(), cc[0].max(): cc[1].min()]
-
-        v_range = np.array([[int(peak3 - v_len), 0], [int(peak3 + v_len), size_v - 1]])
-        data_i_max = np.zeros(v_len * 2)
-        data_i_mean_ = np.zeros(v_len * 2)
-
-        data_i_max[:v_range[1].min() - v_range[0].max()] = data[v_range[0].max(): v_range[1].min(), int(peak2) - 1, int(peak1) - 1]
-
-        data_i_mean = clump_item[v_range[0].max(): v_range[1].min(), bb[0].max(): bb[1].min(), cc[0].max(): cc[1].min()]
-        for i in range(v_range[1].min() - v_range[0].max()):
-            data_i_mean_[i] = data_i_mean[i, ...].mean()
-
-        loc_cen3 = cen3 - v_range[0].max()-1
-        loc_peak3 = peak3 - v_range[0].max()-1
-
-        make_plot_11(clump_item_loc, data_i_max, data_i_mean_, loc_cen3, loc_peak3, title_name, path_save_fig)
-
-
-def display_data(data):
-    if data.ndim == 3:
-        fig = plt.figure(figsize=(15, 8))
-
-        ax1 = fig.add_subplot(1, 3, 1)
-        ax2 = fig.add_subplot(1, 3, 2)
-        ax3 = fig.add_subplot(1, 3, 3)
-        im0 = ax1.imshow(data.sum(0), origin='lower')  # x银纬,y银经
-        im1 = ax2.imshow(data.sum(1), origin='lower')  # x银纬，y速度
-        im2 = ax3.imshow(data.sum(2), origin='lower')  # x银经，y速度
-        return fig, (ax1, ax2, ax3)
-
-
-def display_clumps_fitting(pif_1, df_temp_1, points_all_df, fig_name):
-    data = get_data_points(points_all_df)
-    fig, (ax, ax1, ax2) = display_data(data)
-    ax.scatter(pif_1['Cen3'], pif_1['Cen2'], c='r')
-    ax1.scatter(pif_1['Cen3'], pif_1['Cen1'], c='r')
-    ax2.scatter(pif_1['Cen2'], pif_1['Cen1'], c='r', label='fitting')
-
-    pif_2 = df_temp_1[['Cen1', 'Cen2', 'Cen3']] - points_all_df[['x_2', 'y_1', 'v_0']].values.min(axis=0)
-    ax.scatter(pif_2['Cen3'], pif_2['Cen2'], c='k')
-    ax1.scatter(pif_2['Cen3'], pif_2['Cen1'], c='k')
-    ax2.scatter(pif_2['Cen2'], pif_2['Cen1'], c='k', label='ldc')
-
-    ax2.legend(loc='best', framealpha=0.5)
-    p_1_1, p_1_2, p_2_1, p_2_2 = pif_1['Cen3'].values, pif_2['Cen3'].values, pif_1['Cen2'].values, pif_2[
-        'Cen2'].values
-    for ii in range(p_1_1.shape[0]):
-        ax.plot([p_1_1[ii], p_1_2[ii]], [p_2_1[ii], p_2_2[ii]], 'r')
-
-    p_1_1, p_1_2, p_2_1, p_2_2 = pif_1['Cen3'].values, pif_2['Cen3'].values, pif_1['Cen1'].values, pif_2[
-        'Cen1'].values
-    for ii in range(p_1_1.shape[0]):
-        ax1.plot([p_1_1[ii], p_1_2[ii]], [p_2_1[ii], p_2_2[ii]], 'r')
-
-    p_1_1, p_1_2, p_2_1, p_2_2 = pif_1['Cen2'].values, pif_2['Cen2'].values, pif_1['Cen1'].values, pif_2[
-        'Cen1'].values
-    for ii in range(p_1_1.shape[0]):
-        ax2.plot([p_1_1[ii], p_1_2[ii]], [p_2_1[ii], p_2_2[ii]], 'r')
-
-    fig.savefig(fig_name)
-    plt.close(fig)
-
-
-def make_plot_wcs_1(outcat_wcs, data_wcs, data_cube):
+def make_plot_wcs_1(outcat_wcs, data_wcs, data_cube, fig_name=''):
     """
     在积分图上绘制检测结果
     当没有检测到云核时，只画积分图
@@ -282,8 +152,12 @@ def make_plot_wcs_1(outcat_wcs, data_wcs, data_cube):
     else:
         im0 = axes0.imshow(data_cube)
     if outcat_wcs.values.shape[0] > 0:
-        outcat_wcs_c = SkyCoord(frame="galactic", l=outcat_wcs['Cen1'].values, b=outcat_wcs['Cen2'].values,
-                                unit="deg")
+        if 'Cen1' in outcat_wcs.keys():
+            outcat_wcs_c = SkyCoord(frame="galactic", l=outcat_wcs['Cen1'].values, b=outcat_wcs['Cen2'].values,
+                                    unit="deg")
+        else:
+            outcat_wcs_c = SkyCoord(frame="galactic", l=outcat_wcs['Galactic_Longitude'].values,
+                                    b=outcat_wcs['Galactic_Latitude'].values, unit="deg")
         axes0.plot_coord(outcat_wcs_c, 'r*', markersize=markersize)
 
     axes0.set_xlabel("Galactic Longitude", fontsize=12)
@@ -296,7 +170,11 @@ def make_plot_wcs_1(outcat_wcs, data_wcs, data_cube):
 
     cbar = fig.colorbar(im0, cax=axes1)
     cbar.set_label('K m s${}^{-1}$')
-    plt.show()
+    if fig_name == '':
+        plt.show()
+    else:
+        plt.savefig(fig_name, bbox_inches='tight')
+        plt.close(fig=fig)
 
 
 if __name__ == '__main__':
